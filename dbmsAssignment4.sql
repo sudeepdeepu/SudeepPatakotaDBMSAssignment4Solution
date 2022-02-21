3f3ada898241aa0f6f3ef5b57998a93f0c443535
sudeepdeepu <sudep1997@gmail.com>
2022-02-21 10:42:07 +0530

    Add files via upload

--- dbmsAssignment4.sql
+++ dbmsAssignment4.sql
@@ -50,18 +50,28 @@ select min(price.price) from price where price.bus_type="Sleeper";
 select passenger.passenger_name from passenger where passenger.passenger_name like 'S%';
 
 -- 6)	Calculate price charged for each passenger displaying Passenger name, Boarding City,Destination City, Bus_Type, Price in the output
-
+select passenger.passenger_name,passenger.boarding_city,passenger.destination_city,passenger.bus_type,price.price from passenger inner
+join price on passenger.bus_type=price.bus_type;
 
 -- 7)	What are the passenger name/s and his/her ticket price who travelled in the Sitting bus for a distance of 1000 KM s
+select passenger.passenger_name,price.price from passenger inner
+join price on passenger.bus_type=price.bus_type where passenger.bus_type="Sitting" and passenger.distance=1000;
 
 -- 8)	What will be the Sitting and Sleeper bus charge for Pallavi to travel from Bangalore to Panaji?
+select price.distance,price.price from price inner join passenger on price.bus_type=passenger.bus_type where passenger.passenger_name="Pallavi";
 
 -- 9)	List the distances from the "Passenger" table which are unique (non-repeated distances) in descending order.
 select distinct passenger.distance from passenger order by distance DESC;
 
--- 10)	Display the passenger name and percentage of distance travelled by that passengerfrom the total distance travelled by all passengers without using user variables
+-- 10)	Display the passenger name and percentage of distance travelled by that passenger from the total distance travelled by all passengers without using user variables
+
 
 -- 11)	Display the distance, price in three categories in table Price
 -- a) Expensive if the cost is more than 1000
 -- b) Average Cost if the cost is less than 1000 and greater than 500
-
+select price.distance,price.price,
+CASE
+	WHEN price.price > 1000 THEN 'Expensive'
+    WHEN price.price > 500 THEN 'Average Cost'
+    ELSE 'Cheap cost'
+END AS verdict from price;
